04fd10273d2fb2813fff38eeccae49340e5f5dbd
James <logicism81@gmail.com>
2023-12-16 22:16:45 -0500

    Final Course Version

--- db/sample_db.py
+++ db/sample_db.py
@@ -16,6 +16,11 @@ db = {
       "id": "3",
       "title": "I'm tired...",
       "content": "It's 3am and I want to sleep.",
+    }, 
+    {
+      "id": "4",
+      "title": "Hello world!",
+      "content": "It's 3pm and I want to sleep.",
     }
   ]
 }
@@ -30,6 +35,15 @@ def get_post(post_id):
         if post['id'] == post_id:
             return post
     return None
+
+def update_post(post_id, title, content):
+  global db
+  for post in db['posts']:
+    if post['id'] == post_id:
+      post['title'] = title
+      post['content'] = content
+      return post
+  return None
 
 def add_post(title, content):
 	global db
--- server.py
+++ server.py
@@ -1,50 +1,35 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, Response
-from .db import db
+from flask import Flask, render_template, request
+from .db import sample_db
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html', posts=db.get_posts())
+    return render_template('home/index.html')
 
-@app.route('/post/show')
-def show_post():
-    post_id = request.args.get('id')
-    post = db.get_post(post_id)
-    return render_template('_partials/show.html', post=post)
+@app.route("/posts")
+def posts():
+    posts = sample_db.get_posts()
+    return render_template('posts/index.html', posts=posts)
 
-@app.route('/post/edit', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def edit_post():
-    post_id = request.args.get('id') or request.form.get('id')
-    post = db.get_post(post_id)
+@app.route("/post/<string:post_id>")
+def show_post(post_id):
+    post = sample_db.get_post(post_id)
+    return render_template('posts/_partials/show.html', post=post)
 
-    if request.method == "GET":
-        return render_template('_partials/edit.html', post=post)
-
-    elif request.method == "POST":
+@app.route("/posts/edit/<string:post_id>", methods=['GET', 'PUT'])
+def edit_post(post_id):
+    if request.method == 'PUT':
         title = request.form.get('title')
         content = request.form.get('content')
-        
-        post = db.add_post(title, content)
-        resp = Response(render_template('_partials/show.html', post=post))
-        resp.headers['HX-Trigger'] = "resetForm"
+        sample_db.update_post(post_id, title=title, content=content)
+        return show_post(post_id)
 
-        return resp
+    # Default: GET
+    post = sample_db.get_post(post_id)
+    return render_template('posts/_partials/edit.html', post=post)
 
-    elif request.method == "PUT":
-        title = request.form.get('title')
-        content = request.form.get('content')
-        post = db.set_post(post_id, title, content)
-        return render_template('_partials/show.html', post=post)
-
-    elif request.method == "DELETE":
-        db.delete_post(post_id)
-        return ""
-
-@app.route('/post/validate', methods=['POST'])
-def validate_post():
-    title = request.form.get('title')
-    error = 'Can\'t have digits in the title!' if any(char.isdigit() for char in title) else None
-    return render_template('_partials/validate.html', title=title, error=error)
+@app.route("/post/<string:post_id>/delete", methods=['DELETE'])
+def delete_post(post_id):
+    sample_db.delete_post(post_id)
+    return ""
